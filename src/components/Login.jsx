310db950b770c7cda01b94ce0227d7ecd1180236
Rohit Singh <rohitsingh@Rohits-MacBook-Air-2.local>
2024-02-08 10:17:36 +0530

    ui fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -42,9 +42,9 @@ const Login = () => {
           />
           <p className="text-[20px] text-white ml-[20px]">Base</p>
         </div>
-        <div className="colored-style lg:bock hidden lg:w-[30vw] h-[105vh] bg-white lg:block  overflow-hidden absolute rotate-[8deg] left-[43%] z-10 "></div>
+        <div className="colored-style lg:bock hidden lg:w-[30vw] h-[105vh] bg-[#F8FAFF] lg:block  overflow-hidden absolute rotate-[5deg] left-[44%] z-10 "></div>
 
-        <div className="lg:w-[50%] bg-[#605BFF] hidden lg:block">
+        <div className="lg:w-[55%] bg-[#605BFF] hidden lg:block">
           <img
             src="/icons/app_logo.png"
             alt=""
@@ -76,48 +76,49 @@ const Login = () => {
             />
           </div>
         </div>
-        <div className="h-[100%] px-[5%] lg:bg-white lg:h-[100vh] lg:w-[50vw] lg:px-[5%] py-[7%] z-10">
-          <div className="">
-            <p className="text-[24px] font-[700] mt-[8%]">Sign In</p>
-            <p className="text-[14px] pt-[8px]">Sign in to your account</p>
-          </div>
-          <div className="flex justify-between items-center mt-[30px]">
-            <button
-              type="button"
-              className="text-[#858585] text-[10px] w-[45%]  rounded-lg bg-white hover:bg-[#white]/90  focus:outline-none focus:ring-[#3b5998]/50 px-3 py-2.5 text-center inline-flex items-center mb-2 lg:text-[14px]"
-              onClick={handleGoogleLogin}
-            >
-              <img
-                src="/icons/google-icon.png"
-                className="h-[24px] mr-[10px]"
-                alt=""
-              />
-              Sign in with Google
-            </button>
-            <button
-              type="button"
-              className="text-[#858585] text-[10px] w-[45%] bg-white hover:bg-[#white]/90 focus:ring-4 focus:outline-none  rounded-lg px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mb-2 lg:text-[14px]"
-            >
-              <img
-                src="/icons/apple-icon.png"
-                className="h-[24px] mr-[10px]"
-                alt=""
-              />
-              Sign in with Apple
-            </button>
-          </div>
-          <div className="bg-white rounded-md p-[20px] mt-[20px] py-[40px]">
-            <form action="" onSubmit={handleSubmit}>
-              <label className="text-[16px]  font-lato" htmlFor="email">
-                Email Address
-              </label>
-              <br />
-              <input
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="   
+        <div className="lg:w-[50vw] w-[100vw] bg-[#F8FAFF] flex justify-center">
+          <div className="h-[100vh] w-[100%] px-[5%] bg-[#F8FAFF] lg:bg-[#F8FAFF] flex flex-col justify-center lg:h-[100vh] lg:w-[550px] lg:px-[3%] py-[7%] z-10">
+            <div className="">
+              <p className="text-[24px] font-[700] mt-[8%]">Sign In</p>
+              <p className="text-[14px] pt-[8px]">Sign in to your account</p>
+            </div>
+            <div className="flex justify-between items-center mt-[30px]">
+              <button
+                type="button"
+                className="text-[#858585] text-[10px] w-[45%]  rounded-lg bg-white hover:bg-[#white]/90  focus:outline-none focus:ring-[#3b5998]/50 px-3 py-2.5 text-center inline-flex items-center mb-2 lg:text-[14px]"
+                onClick={handleGoogleLogin}
+              >
+                <img
+                  src="/icons/google-icon.png"
+                  className="h-[24px] mr-[10px]"
+                  alt=""
+                />
+                Sign in with Google
+              </button>
+              <button
+                type="button"
+                className="text-[#858585] text-[10px] w-[45%] bg-white hover:bg-[#white]/90 focus:ring-4 focus:outline-none  rounded-lg px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mb-2 lg:text-[14px]"
+              >
+                <img
+                  src="/icons/apple-icon.png"
+                  className="h-[24px] mr-[10px]"
+                  alt=""
+                />
+                Sign in with Apple
+              </button>
+            </div>
+            <div className="bg-white rounded-md p-[20px] mt-[20px] py-[40px]">
+              <form action="" onSubmit={handleSubmit}>
+                <label className="text-[16px]  font-lato" htmlFor="email">
+                  Email Address
+                </label>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="   
                         mt-[10px]
                         mb-[20px]
                         w-full
@@ -131,18 +132,18 @@ const Login = () => {
                         focus-visible:shadow-none
                         focus:border-primary
                         "
-              />
-              <br />
-              <label className="font-lato" htmlFor="password">
-                Password
-              </label>
-              <br />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="
+                />
+                <br />
+                <label className="font-lato" htmlFor="password">
+                  Password
+                </label>
+                <br />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="
               mt-[10px]
                         w-full
                         rounded-md
@@ -155,47 +156,48 @@ const Login = () => {
                         focus-visible:shadow-none
                         focus:border-primary
                         "
-              />
-              <p className="text-[#346BD4] mt-[10px] text-[14px]">
-                Forgot Passoword?
-              </p>
-              <input
-                type="submit"
-                className="w-[100%] bg-[#605BFF] rounded-md text-white py-[8px] mt-[20px]"
-                value="Sign In"
-              />
-            </form>
-          </div>
+                />
+                <p className="text-[#346BD4] mt-[10px] text-[14px]">
+                  Forgot Passoword?
+                </p>
+                <input
+                  type="submit"
+                  className="w-[100%] bg-[#605BFF] rounded-md text-white py-[8px] mt-[20px]"
+                  value="Sign In"
+                />
+              </form>
+            </div>
 
-          <div className="lg:flex items-center justify-center">
-            <p className="text-center mt-[25px] lg:mt-0 lg:pr-[10px] text-[20px] text-[#858585] font-lato">
-              Don't have an account?
-            </p>
-            <p className="text-[#346BD4] mt-[10px] lg:mt-0 text-[18px] text-center">
-              Register here
-            </p>
-          </div>
-          <div className="flex w-[70%] m-[auto] mt-[40px] justify-between lg:hidden">
-            <img
-              src="/icons/github-mobile.png"
-              alt="Github Icon"
-              className="w-[40px] h-[40px]"
-            />
-            <img
-              src="/icons/twitter-mobile.png"
-              alt="Twitter Icon"
-              className="w-[40px] h-[40px]"
-            />
-            <img
-              src="/icons/linkedin-mobile.png"
-              alt="LinkedIn Icon"
-              className="w-[40px] h-[40px]"
-            />
-            <img
-              src="/icons/discord-mobile.png"
-              alt="Discord Icon"
-              className="w-[40px] h-[40px]"
-            />
+            <div className="lg:flex items-center justify-center">
+              <p className="text-center mt-[25px] lg:mt-0 lg:pr-[10px] text-[20px] text-[#858585] font-lato">
+                Don't have an account?
+              </p>
+              <p className="text-[#346BD4] mt-[10px] lg:mt-0 text-[18px] text-center">
+                Register here
+              </p>
+            </div>
+            <div className="flex w-[70%] m-[auto] mt-[40px] justify-between lg:hidden">
+              <img
+                src="/icons/github-mobile.png"
+                alt="Github Icon"
+                className="w-[40px] h-[40px]"
+              />
+              <img
+                src="/icons/twitter-mobile.png"
+                alt="Twitter Icon"
+                className="w-[40px] h-[40px]"
+              />
+              <img
+                src="/icons/linkedin-mobile.png"
+                alt="LinkedIn Icon"
+                className="w-[40px] h-[40px]"
+              />
+              <img
+                src="/icons/discord-mobile.png"
+                alt="Discord Icon"
+                className="w-[40px] h-[40px]"
+              />
+            </div>
           </div>
         </div>
       </div>
